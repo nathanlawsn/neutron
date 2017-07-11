2ed6f009304cf471ea48dca4689a93900892957a
nathanlawsn <nathan@consultanddesign.com>
2017-07-11 15:34:50 +0100

    Added Neutron base theme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # neutron
-Drupal base theme built for Consult and Design
+Neutron is a Drupal 8 sub-theme of Classy that acts the base theme for all Consult and Design projects.
+
+To use Neutron as your base theme, set the 'base theme' in your theme's .info.yml
+file to "neutron":
+  base theme: neutron
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var importer = require('node-sass-globbing');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var uncss = require('gulp-uncss');
+var stripCssComments = require('gulp-strip-css-comments');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+var shell = require('gulp-shell');
+var sass_config = {
+  importer: importer,
+  includePaths: [
+    'node_modules/breakpoint-sass/stylesheets/',
+    'node_modules/singularitygs/stylesheets/',
+    'node_modules/compass-mixins/lib/'
+  ]
+};
+
+// Compiles Sass
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass(sass_config).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 version']
+    }))
+    .pipe(stripCssComments({preserve: false}))
+    .pipe(cssmin())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css'));
+});
+
+// Uglifies JavaScript
+gulp.task('uglify', function() {
+  return gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('js/minified'));
+});
+
+// Watch for file changes
+gulp.task('watch', function(){
+  livereload.listen();
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./js/*.js', ['uglify']);
+    gulp.watch(['./css/styles.css', './**/*.twig', './js/minified/*.js'], function (files){
+      livereload.changed(files)
+    });
+});
+
+// Run Drush to clear cache
+gulp.task('drush', shell.task([
+  'drush cache-clear'
+]));
